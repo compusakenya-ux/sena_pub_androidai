c908e06d529afd6e02450eddcfa5429bf0fa6ac1
AIRO <compusa.kenya@gmail.com>
2026-08-05 23:07:49 +0300

    Refactor Typography styles in Type.kt
    Updated Typography styles to maintain consistency in font weight.

--- app/src/main/java/com/example/ui/theme/Type.kt
+++ app/src/main/java/com/example/ui/theme/Type.kt
@@ -7,17 +7,20 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
+val Typography =
+  Typography(
+    bodyLarge =
+      TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    ),
+        letterSpacing = 0.5.sp,
+      )
+    /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Bold,
+        fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
@@ -29,4 +32,6 @@ val Typography = Typography(
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     )
-)
+    */
+  )
+
